d10c21d61ea2324e26cf34a6a81f33baa79dba7e
mwcotton <22508607+mwcotton@users.noreply.github.com>
2020-12-17 23:24:42 +0000

    Added running scripts to pipeline.

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tblastn -query enenbeetle.fasta -db longer_contigs/longer_contigs -out longer_hits.txt
+# tblastn -query enenbeetle.fasta -db longer_contigs/longer_contigs -out longer_hits.txt
 
-makeblastdb -in more_contigs.fasta -dbtype nucl -out longer_contigs/longer_contigs
+# makeblastdb -in more_contigs.fasta -dbtype nucl -out longer_contigs/longer_contigs
 
 #initial search
 
@@ -13,3 +13,28 @@ makeblastdb -in more_contigs.fasta -dbtype nucl -out longer_contigs/longer_conti
 #filter top hits
 
 #identify if intro
+
+
+ #add all species to a single fasta file
+
+cat *.fasta >  all_species.fasta
+
+#initial engrailed search
+
+makeblastdb -in all_species.fasta -dbtype nucl -out first_blastdb
+
+tblastn -query engrailed.fasta -db first_blastdb -out prelim_blast.txt
+
+#save top hit for each species
+
+python3 top_contigs.py prelim_blast.txt prelim_top.txt
+
+#search the full contig for top hits
+
+python3 quick_seqs.py prelim_top.txt prelim_contigs.fasta
+
+makeblastdb -in prelim_contigs.fasta -dbtype nucl -out contigs_blastdb
+
+tblastn -query engrailed.fasta -db contigs_blastdb -out contig_blast.txt
+
+#manual search, stitching and reciprocal blast currently required at this point
